59ceca9ef458fd1bdc068f6332e20d68c2297e06
Roman Vaughan <nzsmartie@gmail.com>
2017-02-03 06:21:55 +1300

    Generate a self-signed cert if none is provided (Closes #3)

--- main/component.mk
+++ main/component.mk
@@ -18,3 +18,26 @@ SECRETS := ${COMPONENT_PATH}/secrets
 SECRETS := $(shell cat ${SECRETS} | sed -r 's/^\#.*$$//g; s/^([^=]+)$$/ -D\1/m; s/^([^=]+=)(.*)$$/ -D\1"\2"/' | tr -d '\n')
 
 CFLAGS += $(SECRETS)
+
+#
+# OpenSSL Certificate Generation
+#
+# Since this project uses DTLS, a certificate is needed to present to clients.
+# You may optionally provide your own private key and certificate simply by nameing the files accordingly in the main/ project folder
+#
+# Inspired by https://gist.github.com/ab/4570034
+#
+
+OPENSSL := openssl
+DAYS := -days 365
+KEY_TYPE := rsa:2048
+
+CERT_KEY_FILE := iotnode.key
+CERT_FILE := iotnode.crt
+
+# Since the key is generated along with the crt, no need to specifiy a target for that as well
+$(COMPONENT_PATH)/$(CERT_FILE).txt.o: $(COMPONENT_PATH)/$(CERT_FILE)
+
+$(COMPONENT_PATH)/$(CERT_FILE):
+	@echo Creating a self-signed key certificate pair...
+	$(OPENSSL) req -x509 -nodes -newkey $(KEY_TYPE) -keyout $(COMPONENT_PATH)/$(CERT_KEY_FILE) -out $(COMPONENT_PATH)/$(CERT_FILE) $(DAYS)
